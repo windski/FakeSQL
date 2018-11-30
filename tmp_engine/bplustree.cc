1042c59ccce6647cc30319efb1e35e8a50beff4c
windski <1272162048@qq.com>
2018-12-01 01:07:51 +0800

    modify bplustree struct

--- tmp_engine/bplustree.cc
+++ tmp_engine/bplustree.cc
@@ -1,14 +1,20 @@
 #include "bplustree.h"
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <cstdio>
+#include <cstring>
+#include <cassert>
+
+#include <algorithm>
 
 namespace bplustree {
 
 static const int _NO_PARENT = -1;
+static const int _UNAVALIABLE = -1;
 
     BplusTree::BplusTree()
-        : root_p(0), free_list()
+        : root_p(0), free_list(), cur_parent(nullptr)
     {
         tree_fd = ::open("index.db", O_CREAT | O_RDWR, 0644);
         cur_offset = create_root();
@@ -22,9 +28,20 @@ static const int _NO_PARENT = -1;
     off_t
     BplusTree::create_root()
     {
-        struct tree_node *root = alloc_node(_NO_PARENT);
-        write(tree_fd, root, sizeof(struct tree_node));
-        return ftello(conv2file_p(tree_fd));
+        cur_node = std::make_shared<struct tree_node *>(alloc_node(_NO_PARENT));
+        static char buff[sizeof(struct tree_node) + 1];
+        memcpy(buff, cur_node.get(), sizeof(struct tree_node));
+
+        if (write(tree_fd, buff, sizeof(struct tree_node)) < 0) {
+            perror("tree node create failed");
+            exit(-1);
+        }
+
+        off_t self_offt = ftello(conv2file_p(tree_fd));
+        (*cur_node)->self_ = self_offt;
+        (*cur_node)->leaf_flag_ = true;
+
+        return self_offt;
     }
 
     struct tree_node *
@@ -35,17 +52,55 @@ static const int _NO_PARENT = -1;
         root->parent_ = parent;
         root->next_leaf_ = -1;
 
+        for (int i = 0; i < _CHILDREN_NUMS - 1; i++) {
+            root->keys_[i] = _UNAVALIABLE;
+        }
+
+        for (int i = 0; i < _CHILDREN_NUMS; i++) {
+            root->children_[i] = _UNAVALIABLE;
+        }
+
         return root;
     }
 
 
-    int
-    BplusTree::insert(int value)
+    void
+    BplusTree::insert(int key, char *value)
     {
-        // TODO: complete
-
-        return 0;
+        assert(cur_offset != 0);
+        recurs_insert(key, value);
     }
+
+
+    void
+    BplusTree::recurs_insert(int key, char *value)
+    {
+        int i = 0;
+        while (i < _CHILDREN_NUMS && key >= (*cur_node)->keys_[i]) {
+            if (key == (*cur_node)->keys_[i]) {
+                // 重复, 直接返回
+                return ;
+            }
+
+            i++;
+        }
+
+        if ((*cur_node)->leaf_flag_) {
+            key_insert(key, value, (*cur_node)->self_);
+
+            // TODO: 判断root是否改变, 改变需要重新决定b+tree的根.
+        }
+    }
+
+
+    void
+    BplusTree::key_insert(int key, char *value, off_t parent)
+    {
+        int k = (*cur_node)->keynum_;
+        // TODO:
+    }
+
+
 
 } // end of bplustree
 
--- tmp_engine/bplustree.h
+++ tmp_engine/bplustree.h
@@ -5,19 +5,24 @@
 #include <sys/types.h>
 #include <list>
 
+#include <memory>
+
 
 #define conv2file_p(f) (fdopen((f), "rw"))
 
 
 namespace bplustree {
 
-static const size_t _CHILDREN_NUMS = 100;
+static const int _CHILDREN_NUMS = 100;
 
 
 struct tree_node {
     bool leaf_flag_;
+    off_t self_;
     off_t parent_;
+    int keynum_;
     int keys_[_CHILDREN_NUMS - 1];
+    char values_[_CHILDREN_NUMS][32];
     off_t children_[_CHILDREN_NUMS];
     off_t next_leaf_;
 };
@@ -29,14 +34,18 @@ private:
     off_t cur_offset;
     std::list<off_t> free_list;
     int tree_fd;
+    std::shared_ptr<struct tree_node *> cur_node, cur_parent;
 
     off_t create_root();
     struct tree_node *alloc_node(int parnet);
     int destroy_node(struct tree_node *node);
+
+    void recurs_insert(int key, char *value);
+    void key_insert(int key, char *value, off_t parnet);
 public:
     explicit BplusTree();
     ~BplusTree();
-    int insert(int );
+    void insert(int key, char *value);
     int search(int ) const;
 
 };
